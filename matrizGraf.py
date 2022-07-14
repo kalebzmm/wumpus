fa37511ed35ff0cdbb73a796032ac785d778eddd
ezioalvesfreire <53498926+ezioalvesfreire@users.noreply.github.com>
2022-07-14 01:38:35 -0300

    criado matrizes pseudo aleatórias

--- matrizGraf.py
+++ matrizGraf.py
@@ -1,4 +1,5 @@
 from random import random
+from random import randint
 from random import shuffle
 
 from OpenGL.GL import *
@@ -32,10 +33,22 @@ def re_create():
     colorGSB = (0.83, 1, 0.82)
     colorBreeze = (0.64, 0.76, 0.95)
     colorPit = (0, 0, 0)
+    colorSB = (1, 1, 0.60)
 
+    mapRand = randint(1, 3)
+    print("mapRand", mapRand)
 
-    colorsLRooms = [colorStench, colorWumpus,  colorStench, colorBreeze, colorWhite, colorGSB, colorWhite,
-                    colorPit, colorBreeze, colorPit, colorBreeze, colorBreeze, colorWhite, colorBreeze, colorPit]
+    colorsLRooms = []
+
+    if mapRand == 1:
+        colorsLRooms = [ colorBreeze, colorPit, colorBreeze, colorWhite, colorStench,  colorBreeze, colorPit, colorSB,
+                         colorWumpus, colorStench, colorBreeze,  colorPit,   colorGSB, colorWhite, colorWhite]
+    elif mapRand == 2:
+         colorsLRooms = [colorStench, colorWhite, colorWhite, colorStench, colorWumpus, colorGSB, colorWhite, colorBreeze,
+                         colorSB, colorPit, colorBreeze, colorPit, colorBreeze, colorBreeze, colorWhite]
+    elif mapRand == 3:
+         colorsLRooms = [colorStench, colorWumpus,  colorStench, colorBreeze, colorWhite, colorGSB, colorWhite,
+                         colorPit, colorBreeze, colorPit, colorBreeze, colorBreeze, colorWhite, colorBreeze, colorPit]
     #shuffle(colorsLRooms)
 
 
@@ -57,7 +70,7 @@ def re_create():
         pointInit = (printLRoom * c) +vInit
 
 
-    agente = Image.open("imagens/agente/img.png")
+    agente = Image.open("imagens/agente/arqueiro.png")
     #img_data = numpy.array(list(agente.getdata()), numpy.uint8)
     flipped_image = agente.transpose(Image.FLIP_TOP_BOTTOM)
     img_data = flipped_image.convert("RGBA").tobytes()
